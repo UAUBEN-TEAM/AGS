ea063adc31a47124c268f56f795d6f62380f50b5
genius <issadicko78@gmail.com>
2020-02-08 19:52:55 +0000

    Fix bootstrap errors

--- frontend/AGS/src/app/login/login.component.ts
+++ frontend/AGS/src/app/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  count = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+        this.count++;
+    }, 10);
   }
 
 }
